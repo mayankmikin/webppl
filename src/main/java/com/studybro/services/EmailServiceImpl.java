318756fc848ab199364103fabc40e65f0ba168f1
MAYANK-PC\Mayank Verma <mayank.verma@nviz.com>
2017-06-30 02:06:04 +0530

    abc

--- src/main/java/com/studybro/services/EmailServiceImpl.java
+++ src/main/java/com/studybro/services/EmailServiceImpl.java
@@ -54,7 +54,8 @@ public class EmailServiceImpl implements EmailService
 	    		
 	    		System.out.println("Getting html file");
 	    	//File file= new File(this.getClass().getResource(mailpath).getFile());
-	    	File file =ResourceUtils.getFile("classpath:"+mailpath);
+	    	//File file =ResourceUtils.getFile("classpath:"+mailpath);
+		File file = new File(EmailServiceImpl.class.getClassLoader().getResource(mailpath).getFile());
 	    	String message =FileUtils.readFileToString(file, "UTF-8");
 	    	return message;
 	    	}
