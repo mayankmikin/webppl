d4b8026445ca5c81a645f59a5ad9c302f07b3b39
MAYANK-PC\Mayank Verma <mayank.verma@nviz.com>
2017-07-17 23:54:39 +0530

    adding sbconstatnts

--- src/main/java/com/studybro/controllers/UserController.java
+++ src/main/java/com/studybro/controllers/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.studybro.model.SbConstant;
 import com.studybro.model.User;
 import com.studybro.security.AccountCredentials;
 import com.studybro.services.EmailService;
