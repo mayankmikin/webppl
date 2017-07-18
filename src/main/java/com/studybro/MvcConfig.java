bbba6d18caeda8c68177bf24e1890f005efcd4f3
Upasana08 <upasananagpal08@gmail.com>
2017-07-18 14:43:48 +0530

    adding path locations

--- src/main/java/com/studybro/MvcConfig.java
+++ src/main/java/com/studybro/MvcConfig.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+		"classpath:/META-INF/resources/", "classpath:/resources/",
+		"classpath:/static/", "classpath:/public/" };
+		
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
        
@@ -16,7 +21,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	}
 		if (!registry.hasMappingForPattern("/**")) {
 			registry.addResourceHandler("/**").addResourceLocations(
-					RESOURCE_LOCATIONS);
+					CLASSPATH_RESOURCE_LOCATIONS);
 		}
 		else
 		{
